997f736033ebe82b640b3e6234dec17b2b46b36c
samuel martins <samuel.martins4.sm@gmail.com>
2022-05-28 17:05:08 +0300

    chore: configure database

--- gallery/settings.py
+++ gallery/settings.py
@@ -75,9 +75,11 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'album',
+        'USER': 'samuel',
+        'PASSWORD': 'samuel'
     }
 }
 
