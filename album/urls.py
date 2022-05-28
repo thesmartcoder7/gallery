9f81481ab9ed55968d194045590ad6cdcaa20aee
samuel martins <samuel.martins4.sm@gmail.com>
2022-05-28 20:00:02 +0300

    style: structure and style the landing page

--- album/urls.py
+++ album/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='album_home')
+    path('', views.home, name='album_home'),
+    path('all/', views.all_images, name='album_all')
 ]
--- album/views.py
+++ album/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'album/index.html')
+
+    
+def all_images(request):
+    return render(request, 'album/album.html')
