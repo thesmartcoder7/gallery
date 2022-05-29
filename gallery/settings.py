935246793472b61051192f8c5ecd35b756319e72
samuel martins <samuel.martins4.sm@gmail.com>
2022-05-30 02:34:03 +0300

    patch: fix deployment issues

--- gallery/settings.py
+++ gallery/settings.py
@@ -28,7 +28,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--+euh$sk27k6wfhm6=9wi*^luld3sxw(#^!8t5$wkz$z9(k-(9'
+# SECRET_KEY = 'django-insecure--+euh$sk27k6wfhm6=9wi*^luld3sxw(#^!8t5$wkz$z9(k-(9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG =  True
