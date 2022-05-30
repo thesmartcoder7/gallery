bfd5bb661c6c5557e1c3e36fa72a33940101bfd5
samuel martins <samuel.martins4.sm@gmail.com>
2022-05-30 03:15:38 +0300

    patch: fix deployment issues

--- gallery/settings.py
+++ gallery/settings.py
@@ -143,9 +143,9 @@ USE_TZ = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
-MEDIA_URL = 'media/'
+STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static/" ),
