ee76d4741b158f8ee4e3df9a63733613c5efcc8c
samuel martins <samuel.martins4.sm@gmail.com>
2022-05-30 03:39:34 +0300

    patch: fix deployment issues

--- gallery/urls.py
+++ gallery/urls.py
@@ -27,8 +27,8 @@ urlpatterns = [
     path('album/', include('album.urls'))
 ]
 
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'album.views.handle_not_found'
