04af6e70627033b0e55c6ebf114836cae394cb00
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-06-25 23:38:27 +0300

    docs: document the album models

--- album/models.py
+++ album/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Location(models.Model):
+    """
+    Model representing a location.
+
+    Attributes:
+        name (str): The name of the location.
+
+    Methods:
+        save(self, *args, **kw): Overrides the save method to add custom behavior.
+        delete(self, *args, **kw): Overrides the delete method to add custom behavior.
+        __str__(self): Returns a string representation of the location.
+        update(cls, id, value): Updates the name of the location with the specified ID.
+
+    """
+
     name = models.CharField(max_length=100)
 
     def save(self, *args, **kw):
@@ -21,6 +33,20 @@ class Location(models.Model):
 
 
 class Category(models.Model):
+    """
+    Model representing a category.
+
+    Attributes:
+        name (str): The name of the category.
+
+    Methods:
+        save(self, *args, **kw): Overrides the save method to add custom behavior.
+        delete(self, *args, **kw): Overrides the delete method to add custom behavior.
+        __str__(self): Returns a string representation of the category.
+        update(cls, id, value): Updates the name of the category with the specified ID.
+
+    """
+
     name = models.CharField(max_length=100)
 
     def save(self, *args, **kw):
@@ -41,6 +67,27 @@ class Category(models.Model):
 
 
 class Image(models.Model):
+    """
+    Model representing an image.
+
+    Attributes:
+        image (ImageField): The uploaded image file.
+        name (str): The name of the image.
+        description (str): The description of the image.
+        location (ForeignKey): The associated location.
+        category (ForeignKey): The associated category.
+
+    Methods:
+        save(self, *args, **kw): Overrides the save method to add custom behavior.
+        delete(self, *args, **kw): Overrides the delete method to add custom behavior.
+        __str__(self): Returns a string representation of the image.
+        update(cls, id, name, description, category, location): Updates the image with the specified ID and attributes.
+        get_image_by_id(cls, id): Retrieves the image with the specified ID.
+        search_by_category(cls, category): Searches for images based on the specified category.
+        filter_by_location(cls, location): Filters images based on the specified location.
+
+    """
+
     image = models.ImageField(upload_to='uploads/')
     name = models.CharField(max_length=100)
     description = models.TextField()
